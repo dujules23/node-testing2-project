44cb998ac46712199e2499e5d9ca3344c6e67476
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-09-09 12:21:24 -0400

    setup server and knex file, added other dependencies

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+// Update with your config settings.
+
+const common = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: '.data/seeds' },
+}
+
+module.exports = {
+
+  development: {
+    ...common,
+    connection: {
+      filename: './data/characters.db3'
+    }
+  },
+
+  staging: {
+    ...common,
+    connection: {
+      filename: './data/test.db3'
+    }
+   
+  },
+
+  production: {
+  
+  }
+
+};
