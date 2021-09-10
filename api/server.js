247b3b50c283880490887bf6bf1f1a1672344cdb
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-09-09 20:53:22 -0400

    completed test suite for server, works as intended

--- api/server.js
+++ api/server.js
@@ -4,11 +4,11 @@ const Characters = require("./characters/characters-model")
 
 const server = express();
 
-const charactersRouter = require("./characters/characters-router")
+
 
 
 server.use(express.json());
-server.use("/api/characters", charactersRouter)
+
 
 server.get("/", (req,res) =>{
   res.status(200).json("Server is working")
@@ -24,4 +24,28 @@ server.get("/characters", (req,res) => {
     })
 })
 
+server.post("/characters", (req, res) => {
+  Characters.insert(req.body)
+    .then(character => {
+      res.status(201).json(character)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+server.delete("/characters/:id", (req, res) => {
+  Characters.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end();
+      }
+      else {
+        res.status(404).json({message: "Record not Found"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: `server error ${err}`})
+    })
+})
 module.exports = server
